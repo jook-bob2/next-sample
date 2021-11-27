dc4d0156ffcce9b94dbe8a800ed58fc3ff75081b
김진석 <jskim91@starlabs.co.kr>
2021-11-28 00:54:09 +0900

    update

--- components/dashboard/main/todaysPowerPlant/byPowerPlant/inverterStatus/TodaysInverterStatus.jsx
+++ components/dashboard/main/todaysPowerPlant/byPowerPlant/inverterStatus/TodaysInverterStatus.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export default function TodaysInverterStatus() {
 	return (
 		<header style={{ border: 'solid 1px red' }}>
-			<Link href={{ pathname: '/' }}>
+			<Link href={{ pathname: '/inverter/inverter-info' }}>
 				<a>인버터 화면으로 이동</a>
 			</Link>
 			<h1>인버터 상태</h1>
